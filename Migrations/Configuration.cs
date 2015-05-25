44716dad79311a9009cf6c58f9bf3c51869afc6b
Victor Antofica <victorantos@gmail.com>
2015-05-26 00:15:32 +0100

    add default dbinitializer - a couple of job posts

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -26,23 +26,7 @@ namespace AngJobs.Migrations
             //      new Person { FullName = "Brice Lambson" },
             //      new Person { FullName = "Rowan Miller" }
             //    );
-
-
-            context.jobPosts.AddOrUpdate(
-              p => p.JobTitle,
-              new JobPost { JobTitle = "Senior Front End/JavaScript Developer - £60k - ReactJS",
-                            JobDescription="My customer started his business when he was 15 from home and it has since become the world leader in their field. Still running the company from the top and with a recent investment from one of the worlds largest .coms he is looking to expand the business even further and is so doubling the team his London based team.",
-                            IsOnFrontPage=true
-              },
-              new JobPost { JobTitle = "UI Developer - AngularJS - Javascript - JQuery HTML5 - CSS3 - Typescript - ASP.NET - Warrington",
-                            JobDescription = "UI Developer with AngularJS, JavaScript, JQuery, HTML5, CSS3, experience is sought for six month contract in Warrington. Typescript and ASP.NET experience is desired but not essential.",
-                            IsOnFrontPage = true
-              },
-              new JobPost { JobTitle = "Technical Lead - Startup - London",
-                            JobDescription = "Full Stack Technical Lead - Startup - London - £75000 - 95000 per annum, + Equity",
-
-              }
-            );
+             
         }
     }
 }
--- Models/DataModels.cs
+++ Models/DataModels.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 
 namespace AngJobs.Models
 {
@@ -63,6 +65,27 @@ namespace AngJobs.Models
         protected override void Seed(DBContext context)
         {
             //Create an seed data you wish in the database.
+            context.jobPosts.AddOrUpdate(
+           p => p.JobTitle,
+           new JobPost
+           {
+               JobTitle = "Senior Front End/JavaScript Developer - £60k - ReactJS",
+               JobDescription = "My customer started his business when he was 15 from home and it has since become the world leader in their field. Still running the company from the top and with a recent investment from one of the worlds largest .coms he is looking to expand the business even further and is so doubling the team his London based team.",
+               IsOnFrontPage = true
+           },
+           new JobPost
+           {
+               JobTitle = "UI Developer - AngularJS - Javascript - JQuery HTML5 - CSS3 - Typescript - ASP.NET - Warrington",
+               JobDescription = "UI Developer with AngularJS, JavaScript, JQuery, HTML5, CSS3, experience is sought for six month contract in Warrington. Typescript and ASP.NET experience is desired but not essential.",
+               IsOnFrontPage = true
+           },
+           new JobPost
+           {
+               JobTitle = "Technical Lead - Startup - London",
+               JobDescription = "Full Stack Technical Lead - Startup - London - £75000 - 95000 per annum, + Equity",
+
+           }
+         );
         }
     }
 }
