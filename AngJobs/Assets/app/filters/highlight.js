904813aa10affc2f90767844b764020efd62e49b
Victor Antofica <hello@victorantos.com>
2015-06-18 10:41:41 +0100

    add highlight for search filter

---
+++ AngJobs/Assets/app/filters/highlight.js
@@ -0,0 +1,7 @@
+﻿app.filter('highlight', ['$sce', function ($sce) {
+    return function (text, phrase) {
+        if (phrase)
+            text = text.replace(new RegExp('(' + phrase + ')', 'gi'), '<span style="background-color:yellow">$1</span>');
+        return $sce.trustAsHtml(text);
+    }
+}]);
