4ece5ca8322ac328986102a69e66f468fc09606d
victorantos <github@victorantos.com>
2016-02-13 14:00:57 +0000

    Add angular 2 lib

---
+++ AngJobs/Assets/app2/main.ts
@@ -0,0 +1,9 @@
+﻿import {bootstrap} from "angular2/platform/browser";
+import {Component} from "angular2/core";
+@Component({
+    selector: 'app',
+    template: '<div>Hello World</div>'
+})
+class App { }
+
+bootstrap(App);
