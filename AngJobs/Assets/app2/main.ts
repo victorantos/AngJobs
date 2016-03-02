e69cad5f383ca3f06b3a4d37e4ff761c99aa3882
victorantos <github@victorantos.com>
2016-03-02 15:40:20 +0000

    Fix build and out of memory exception

--- AngJobs/Assets/app2/main.ts
+++
@@ -1,9 +0,0 @@
-﻿import {bootstrap} from "angular2/platform/browser";
-import {Component} from "angular2/core";
-@Component({
-    selector: 'my-app',
-    template: '<div>Hello World</div>'
-})
-class App { }
-
-bootstrap(App);
--- AngJobs/Helpers/Helpers.cs
+++ AngJobs/Helpers/Helpers.cs
@@ -78,7 +78,7 @@ namespace Angjobs.Helpers
 
         public static object GetAllJobPostsShortDescription(DBContext db, int maxJobs)
         {
-            var list = db.JobPosts.AsNoTracking().Where(j => (!j.IsDeleted.HasValue || !j.IsDeleted.Value)).ToList().Select(j => new JobPostViewModel(j)).OrderByDescending(j => j.dateCreated).Take(maxJobs).OrderByDescending(j => j.datePosted).OrderByDescending(c => c.priority).ToList();
+            var list = db.JobPosts.AsNoTracking().Where(j => (!j.IsDeleted.HasValue || !j.IsDeleted.Value)).Take(maxJobs).ToList().Select(j => new JobPostViewModel(j)).OrderByDescending(j => j.dateCreated).OrderByDescending(j => j.datePosted).OrderByDescending(c => c.priority).ToList();
 
             foreach (var item in list)
             {
