8c9f6814a5a4e0fa13246920d7471316ea2dc8d9
victorantos <github@victorantos.com>
2016-03-21 09:33:17 +0000

    Display the job line correctly when job title includes a url

--- AngJobs/Assets/app/directives/jobLine.js
+++ AngJobs/Assets/app/directives/jobLine.js
@@ -12,17 +12,20 @@
     var links = new Array();
    
     var firstLinkIndex = $scope.jobTitle.indexOf('<a');
-    if (firstLinkIndex > -1) {
-        var link1 = $scope.jobTitle.substring(0, firstLinkIndex);
-        links.push(new Line(link1, '#!/jobdetails/' + $scope.jobId));
+    var endFirstLinkIndex = $scope.jobTitle.indexOf('</a>');
 
-        var firstEndLinkIndex = $scope.jobTitle.indexOf('</a') +4;
-        var link2 =  $($scope.jobTitle.substring(firstLinkIndex, firstEndLinkIndex))[0];
-        links.push(new Line(link2.innerText.replace("http://", "").replace("https://", ""), link2.href));
+    //make sure the link has also the ending tag
+    if (firstLinkIndex > -1 && endFirstLinkIndex > -1 && endFirstLinkIndex > firstLinkIndex) {
+            var link1 = $scope.jobTitle.substring(0, firstLinkIndex);
+            links.push(new Line(link1, '#!/jobdetails/' + $scope.jobId));
 
-        var link3 = $scope.jobTitle.substring(firstEndLinkIndex);
-        links.push(new Line(link3, '#!/jobdetails/' + $scope.jobId));
+            var firstEndLinkIndex = $scope.jobTitle.indexOf('</a') + 4;
+            var link2 = $($scope.jobTitle.substring(firstLinkIndex, firstEndLinkIndex))[0];
+            links.push(new Line(link2.innerText.replace("http://", "").replace("https://", ""), link2.href));
 
+            var link3 = $scope.jobTitle.substring(firstEndLinkIndex);
+            links.push(new Line(link3, '#!/jobdetails/' + $scope.jobId));
+      
     }
     else
     {
