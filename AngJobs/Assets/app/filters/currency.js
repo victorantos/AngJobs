e8d11754bcc8ca4441f922251876fbde1be4abb5
palsaxo <pal@saxobank.com>
2016-03-21 22:32:10 +0530

    using utils-service to get currency symbol

--- AngJobs/Assets/app/filters/currency.js
+++ AngJobs/Assets/app/filters/currency.js
@@ -1,11 +1,14 @@
-﻿app.filter('currency', function () {
+﻿app.filter('currency', ['utils', function (utils) {
+    var _this = this;
+    this.utils = utils;
     return function (currencyCode) {
+        _this.utils;
         if (currencyCode === 0) {
-            return '£';
+            return _this.utils.CurrencySymbol[0];
         } else if (currencyCode === 1) {
-            return '€';
+            return _this.utils.CurrencySymbol[1];
         } else if (currencyCode === 2) {
-            return '$';
+            return _this.utils.CurrencySymbol[2];
         }
     }
-})
+}]);
