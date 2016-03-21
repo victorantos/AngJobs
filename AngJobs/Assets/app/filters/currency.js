e524ae4081b3a03fc52a65317ddaa14160e36116
palsaxo <pal@saxobank.com>
2016-03-21 21:18:07 +0530

    use currency filter to transform currrency numeric code

---
+++ AngJobs/Assets/app/filters/currency.js
@@ -0,0 +1,11 @@
+﻿app.filter('currency', function () {
+    return function (currencyCode) {
+        if (currencyCode === 0) {
+            return '£';
+        } else if (currencyCode === 1) {
+            return '€';
+        } else if (currencyCode === 2) {
+            return '$';
+        }
+    }
+})
