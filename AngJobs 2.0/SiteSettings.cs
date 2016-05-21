1e1d556ac5dd92d9c9e5d5bef3bf055ba2b13f0d
victorantos <github@victorantos.com>
2016-05-21 10:12:07 +0100

    remove .NET core RC1 code

--- AngJobs 2.0/SiteSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace AngJobs
-{
-    public class SiteSettings
-    {
-
-    }
-}
--- AngJobs 2.0/wwwroot/app/bootstrap.ts
+++
@@ -1,6 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import { App } from './app';
-
-bootstrap(App, [])
-  .then(success => console.log(`Bootstrap success`))
-  .catch(error => console.log(error));
