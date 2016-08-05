ffa4586792f44fd8b7cb38fb8342442c6a816969
victorantos <github@victorantos.com>
2016-08-05 11:42:23 +0100

    Add  basic apply form layout

---
+++ AngJobs2/ClientApp/components/job-detail/apply-now.ts
@@ -0,0 +1,19 @@
+﻿import * as ng from "@angular/core";
+
+@ng.Component(
+    {
+        selector: "apply-now",
+        template:  require('./apply-now.html')
+        
+    })
+export class ApplyNow {
+    @ng.Input() applied: boolean;
+    @ng.Output() appliedChange = new ng.EventEmitter();
+
+   jobapplicationMsg: string;
+    applyForJob()
+    {
+        this.appliedChange.emit(false);
+        console.log(this.jobapplicationMsg);
+    }
+}
--- AngJobs2/ClientApp/components/job-detail/job-detail.ts
+++ AngJobs2/ClientApp/components/job-detail/job-detail.ts
@@ -1,10 +1,12 @@
 ﻿import * as ng from '@angular/core';
 import { Http } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import {ApplyNow} from './apply-now';
 
 @ng.Component({
     selector: 'job-detail',
-    template: require('./job-detail.html')
+    template: require('./job-detail.html'),
+    directives: [ApplyNow]
 })
 export class JobDetail implements ng.OnInit {
     @ng.Input('job-id') jobId: number;
