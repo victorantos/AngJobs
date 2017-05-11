228fc9c783303dfd22473242fa9d3536062ca9a0
victor <hello@victorantos.com>
2017-05-11 16:02:31 +0100

    Prepare job application, with uploded files, for storing into DB

--- AngJobs/ClientApp/app/components/jobs/jobapplication.dialog.ts
+++ AngJobs/ClientApp/app/components/jobs/jobapplication.dialog.ts
@@ -1,5 +1,5 @@
 ﻿import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
-import { Component, Input, Inject } from "@angular/core";
+import { Component, Input, Inject, OnInit } from "@angular/core";
 import { FileSelectDirective, FileUploader, Headers } from 'ng2-file-upload/ng2-file-upload';
 
 
@@ -7,20 +7,24 @@ const URL = '/api/upload';
 
 @Component({
     selector: 'job-application-dialog',
-    templateUrl: './jobapplication.dialog.html',
+    templateUrl: './jobapplication.dialog.html'
 })
-export class JobApplicationDialog {
+export class JobApplicationDialog implements OnInit {
     public jobId: number;
     public uploader: FileUploader = new FileUploader({ url: URL, autoUpload: true });
     public fullName: string;
     public emailAddress: string;
     public message: string;
+    public uploadIds: Array<string> = new Array();
 
     constructor(public dialogRef: MdDialogRef<JobApplicationDialog>, @Inject(MD_DIALOG_DATA) public data: any) {
-
         this.jobId = data.jobId;
-
-        console.log("jobid:", data);
+        this.uploader.options.url += '?jobId=' + this.jobId; 
     }
-     
+
+    ngOnInit() {
+        this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+            this.uploadIds.push(response);
+        };
+    }
 }
--- AngJobs/Startup.cs
+++ AngJobs/Startup.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
 namespace AngJobs
 {
     public class Startup
@@ -61,7 +62,7 @@ namespace AngJobs
                 routes.MapSpaFallbackRoute(
                     name: "spa-fallback",
                     defaults: new { controller = "Home", action = "Index" });
-            });
+            }); 
         }
     }
 }
