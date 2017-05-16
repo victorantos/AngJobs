e5b8abc1a76d5fb49ed542e79774b15a11d65d95
victor <hello@victorantos.com>
2017-05-16 11:59:44 +0100

    Remove unused projects

--- AngJobs.Android/Assets/app/controllers/homeCtrl.js
+++
@@ -1,6 +0,0 @@
-﻿angular.module('home', [])
-    .controller('homeCtrl',['$scope','$http', function ($scope, $http) {
-        $scope.alert = function () {
-            alert("WOW");
-        }
-    }]);
--- Owin.Security.Providers/Buffer/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Owin.Security.Providers.Buffer
-{
-    internal static class Constants
-    {
-        public const string DefaultAuthenticationType = "Buffer";
-    }
-}
