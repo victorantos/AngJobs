17e2a3d7b27b0e954e48ebf8a530ad10079ecb60
Victor A <victorantos@users.noreply.github.com>
2021-04-04 07:19:14 +0100

    Add prod setting

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  whoishiringuserUrl: "https://hacker-news.firebaseio.com/v0/user/whoishiring.json",
+  whoishiringitemUrl: "https://hacker-news.firebaseio.com/v0/item/{id}.json",
 };
