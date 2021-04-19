ac9cc8a02c6fdfcb54cb7e0082198ec322d1761d
Victor A <github@victorantos.com>
2021-04-19 06:12:53 +0100

    Add controller

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -4,36 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using AngJobs.Models;
 namespace AngJobs.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class JobsController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<JobsController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public JobsController(ILogger<JobsController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<FavouriteJobs> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new FavouriteJobs
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Id = Guid.NewGuid(),
+                JobIds = new List<int>{
+                    rng.Next(2000000),
+                    rng.Next(2000000),
+                    rng.Next(2000000),
+                    rng.Next(2000000)
+                }
+            }).ToArray();
         }
     }
 }
