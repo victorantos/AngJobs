9ad40f6475d7900ef86625efb762de3d543e8740
Victor A <victorantos@users.noreply.github.com>
2024-09-30 15:41:54 +0200

    Fix prop. acess using frontmatter

--- vuepress-plugin-hello/JobApplication.vue
+++ vuepress-plugin-hello/JobApplication.vue
@@ -102,8 +102,8 @@
         formData.append("text", this.form.message);
         formData.append("url", document.URL);
         formData.append("title", document.title);
-        formData.append("source", $page.author.url);
-        formData.append("author", $page.author.name);
+        formData.append("source", $page.frontmatter.author.url);
+        formData.append("author", $page.frontmatter.author.name);
 
    
         // Send POST request to the API
